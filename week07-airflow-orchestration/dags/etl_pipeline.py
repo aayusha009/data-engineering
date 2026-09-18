b14daca27f99481e0b6126dc0d96b4c077969028
Aayusha <aayushad08@gmail.com>
2026-09-18 07:24:49 +0545

    week 7 practical work

--- week07-airflow-orchestration/dags/etl_pipeline.py
+++ week07-airflow-orchestration/dags/etl_pipeline.py
@@ -5,22 +5,25 @@ from datetime import datetime, timedelta
 def extract_from_API():
     print("Data extracted from API")
 
-def land_raw_data():
-    print("Landing raw data into raw storage")
+def land_raw_data(**kwargs):
+    execution_date = kwargs['ds']
+    print(f"Landing raw data for {execution_date} - writing to raw/{execution_date}.json ")
 
-def load_from_warehouse():
-    print("Data loaded into the warehouse")
+def load_from_warehouse(**kwargs):
+    execution_date = kwargs['ds']
+    print(f"Loading warehouse data for {execution_date} - using INSERT ... ON CONFLICT (upsert) keyed on date, so re-running updates the existing row instead of inserting a duplicate")
 
 def dbt_transformation():
     print("data transformed using dbt")
 
 with DAG(
-    dag_id = "etl_pipeline", #unique name for the DAG 
+    dag_id = "etl_pipeline",
     start_date = datetime(2026,1, 1),
-    schedule = None #won't run automatically, only when i trigger it manually, it will run
+    schedule = None,
+    catchup = False  # Airflow ignores all the missed history. No historical backfills automatically  
 ) as dag:
 
-    extract_task = PythonOperator(task_id = "extract", python_callable = extract_from_API)
+    extract_task = PythonOperator(task_id = "extract", python_callable = extract_from_API, retries = 2, retry_delay = timedelta(minutes = 1))
     land_task = PythonOperator(task_id = "land", python_callable = land_raw_data)
     load_task = PythonOperator(task_id = "load", python_callable = load_from_warehouse)
     dbt_task= PythonOperator(task_id = "dbt", python_callable = dbt_transformation)
